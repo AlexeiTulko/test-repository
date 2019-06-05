0753af6197c5a0b6a113463401efff12a911e251
AlexeiTulko <alexeitulko@gmail.com>
2019-06-05 23:15:21 +0300

    homework
    Signed-off-by: AlexeiTulko <alexeitulko@gmail.com>

---
+++ kalendar.py
@@ -0,0 +1,22 @@
+import calendar
+
+
+Y = int(input('Year = '))
+M = int(input('Month = '))
+D = int(input('Day = '))
+
+x = calendar.weekday(Y, M, D)
+if x == 0:
+    print('Monday')
+elif x == 1:
+    print('Tuesday')
+elif x == 2:
+    print('Wednesday')
+elif x == 3:
+    print('Thursday')
+elif x == 4:
+    print('Friday')
+elif x == 5:
+    print('Saturday')
+else:
+    print('Sunday')
