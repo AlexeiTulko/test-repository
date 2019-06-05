7b6c44462f272d3c321f811456605668453bdaed
AlexeiTulko <alexeitulko@gmail.com>
2019-06-05 23:15:21 +0300

    homework
    Signed-off-by: AlexeiTulko <alexeitulko@gmail.com>

--- zero.py
+++ zero.py
@@ -2,6 +2,7 @@
 numbers = [] #zeros to place in the beginning
 for i in range(int(input('numbers = '))):
     numbers.append(int(input('{}number = '.format(i+1))))
-numbers1 = sorted(numbers) #не понимаю, как перенести нули, не отсортировав список
+numbers1 = ([k for k in numbers if k==0])+([k for k in numbers if k!=0])
 print(numbers)
 print(numbers1)
+
