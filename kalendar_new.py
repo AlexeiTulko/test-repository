9587c4471adfe12f856d1aef45705d1083f63a1e
AlexeiTulko <alexeitulko@gmail.com>
2019-06-12 23:44:49 +0300

    homework_2(2) календарь 2
    Signed-off-by: AlexeiTulko <alexeitulko@gmail.com>

--- kalendar_new.py
+++ kalendar_new.py
@@ -1,7 +1,22 @@
 import calendar
 
-day = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday',
-       3: 'Thursday', 4: 'Friday',
-       5: 'Saturday', 6: 'Sunday'}
 
-print(day[calendar.weekday(int(input('Год = ')), int(input('Month = ')), int(input('Day = ')))])
+def search(year, month, dd):
+    while year >= 1970 and 1 <= month <= 12 and dd == 1:
+        yield year, month, dd
+        month -= 1
+        if not month:
+            year -= 1
+
+
+latest_year, latest_month, ddd = 2019, 6, 1
+your_day = input('Введите день = ')
+day = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2,
+       'Thursday': 3, 'Friday': 4,
+       'Saturday': 5, 'Sunday': 6}
+
+for i in search(latest_year, latest_month, ddd):
+    if calendar.weekday(*i) == day[your_day]:
+        print(*i[:2])
+        break
+
