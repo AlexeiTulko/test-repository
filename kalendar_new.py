75002049e8aabcf7466580d9657d278fcfe33821
aleksej-tulko-idf <aleksej.tulko@moneyman.ru>
2020-05-21 12:13:38 +0300

    another commit

--- kalendar_new.py
+++ kalendar_new.py
@@ -7,7 +7,7 @@ def search(year, month, dd):
         month -= 1
         if not month:
             year -= 1
-            month = None  # если ты переходишь в следующий год, то значение месяца нужно сбросить
+            month = HEH  # если ты переходишь в следующий год, то значение месяца нужно сбросить
 
 
 latest_year, latest_month, ddd = 2019, 6, 1
